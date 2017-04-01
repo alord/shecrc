7c94aef0d390940863dca426d5d934f6529c5a54
Alexandre Lord <alexandre.lord@gmail.com>
2017-04-01 09:15:15 -0400

    Set working path (consider switching to a pwd later)

--- start.ps1
+++ start.ps1
@@ -4,7 +4,7 @@
 docker network create shecrc
 
 docker create --net shecrc -p 3306:3306 --name shecrc-mysql -e MYSQL_ROOT_PASSWORD=docker-mysql -e MYSQL_DATABASE=shecrc_drupal -e MYSQL_USER=shecrcdrupal -e MYSQL_PASSWORD=shecrcdrupal shecrc/mysql
-docker create --net shecrc -p 8080:80 --name shecrc-apache shecrc/apache-php
+docker create --net shecrc -p 8080:80 --name shecrc-apache -v C:\dev\shecrc\src:"/var/www/html" shecrc/apache-php
 
 docker start shecrc-mysql
 docker start shecrc-apache
